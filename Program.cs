07fddd9fd6adc22e80e34500524ee8128a260817
MustafaElyesaUzun <mustafa.elyesa.uzun@hotmail.com>
2023-03-26 18:44:09 +0300

    feat: add divide function

--- Program.cs
+++ Program.cs
@@ -23,6 +23,9 @@
                 case "-":
                     Console.WriteLine(sayi1 - sayi);
                     break;
+                case "*":
+                    Console.WriteLine(sayi1 * sayi);
+                    break;
             }
 
         }
